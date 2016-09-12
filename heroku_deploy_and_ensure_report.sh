8e905641f2507af36ecc24ceb0d737efbad49153
Kenichi Kamiya <kachick1@gmail.com>
2016-09-12 14:58:32 +0900

    Adjust for GNU grep and relax regex

--- heroku_deploy_and_ensure_report.sh
+++ heroku_deploy_and_ensure_report.sh
@@ -2,7 +2,7 @@ heroku_deploy_and_ensure_report() {
   local report=$(git push heroku master)
   local result=$?
 
-  echo "$report" | grep -F 'remote: Verifying deploy... done.' >/dev/null
+  echo "$report" | grep -P 'remote: Verifying deploy\.+?done|Everything up-to-date' >/dev/null
   if [ $? -ne 0 ]; then
     result=1
   fi
