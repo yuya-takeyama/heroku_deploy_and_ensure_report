a8b0d82c991f2abb7fc466d3a593d524c65e5dd5
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2016-09-14 13:43:29 +0900

    Show command output periodically

--- heroku_deploy_and_ensure_report.sh
+++ heroku_deploy_and_ensure_report.sh
@@ -1,11 +1,32 @@
 heroku_deploy_and_ensure_report() {
-  local deploy_command="$1"
-  local report=`$deploy_command`
-  local result=$?
+  local self_name=${FUNCNAME[0]}
+  local tmpfile=mktemp
+  local deploy_command=$@
+  local exit_status=0
 
-  if ! echo "$report" | grep -sqP 'remote: Verifying deploy\.+?done|Everything up-to-date'; then
-    result=1
+  echo "${self_name}: Running command: ${deploy_command}" >&2
+
+  $deploy_command | tee $tmpfile
+  local deploy_result=${PIPESTATUS[0]}
+
+  if [ ! $deploy_result == 0 ]; then
+    exit_status=$deploy_result
+  else
+    if ! grep -sq 'remote: Verifying deploy\.+?done|Everything up-to-date' "${tempfile}"; then
+      exit_status=1
+    fi
+
   fi
 
-  return $result
+  rm $tmpfile
+
+  echo "${self_name}: Command exited with ${deploy_result}" >&2
+
+  if [ $exit_status == 0 ]; then
+    echo "${self_name}: Successfully deployed!" >&2
+  else
+    echo "${self_name}: Failed to deploy..." >&2
+  fi
+
+  return $exit_status
 }
